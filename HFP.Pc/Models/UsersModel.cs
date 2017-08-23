a1a4801ac6e03608b9eb9a044f8855fd0cd90f40
cx <18311076836@126.com>
2017-08-23 22:05:03 +0800

    新增model

---
+++ HFP.Pc/Models/UsersModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HFP.Pc.Models
+{
+    public class UsersModel
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public string Phone { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
